365b0d8ed217e23b7d74ed3fd322a1e6ff59db6d
Rafael Torres <rdtr.sis@gmail.com>
2019-06-16 11:40:36 -0400

    ADD: * circleCI integration. * gitignore rules. * requirements packages.

---
+++ sample.py
@@ -0,0 +1,5 @@
+import django
+
+def fr():
+    print('ggg')
+
---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from setuptools import find_packages, setup
+
+PROJECT_NAME = "minesweeper_api"
+VERSION = "0.1"
+
+setup(
+    name=PROJECT_NAME,
+    version=VERSION,
+    description="An minesweeper api",
+    author="Rafael Torres",
+    author_email="rdtr.sis@gmail.com",
+    url="https://github.com/rafasis1986/minesweeper-API",
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    include_package_data=True,
+    package_data={PROJECT_NAME: ['static/*.*', 'templates/*.*']},
+    scripts=['manage.py'])
