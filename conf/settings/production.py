728139b592a2ff5dc8712334354fff6c91790606
Rafael Torres <rdtr.sis@gmail.com>
2019-06-18 10:19:44 -0400

    ADD: dockerize production environment

--- conf/settings/production.py
+++ conf/settings/production.py
@@ -1,7 +1,32 @@
 import os
 
+import dj_database_url
+
 from .base import * # noqa
 
+
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
-ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=int(os.getenv('POSTGRES_CONN_MAX_AGE', 60)),
+    ),
+}
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': os.getenv('REDIS_URL'),
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+            'IGNORE_EXCEPTIONS': True,
+        },
+    },
+}
+
+EMAIL_SUBJECT_PREFIX = os.getenv(
+    'DJANGO_EMAIL_SUBJECT_PREFIX', '[Minesweeper-API]',
+)
+
 INSTALLED_APPS += ['gunicorn']  # noqa
--- conf/wsgi.py
+++ conf/wsgi.py
@@ -1,7 +1,12 @@
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
+app_path = os.path.abspath(
+    os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir),
+)
+sys.path.append(os.path.join(app_path, 'api'))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'conf.settings.production')
 
